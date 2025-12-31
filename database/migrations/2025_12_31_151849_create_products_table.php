2e8e389d284eb641373d7bc56f9efa5407a1ae9a
sureshkumarks1 <sureshkumarks1@gmail.com>
2025-12-31 20:54:14 +0530

    schema created

--- database/migrations/2025_12_31_151849_create_products_table.php
+++ database/migrations/2025_12_31_151849_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('store_id')->constrained();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
             $table->timestamps();
         });
     }
